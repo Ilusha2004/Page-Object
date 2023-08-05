42eabcf9458ad3b9267231114e08ddcdac5e3e56
KoVLya <ilakovunov@gmail.com>
2023-08-05 14:16:36 +0300

    hot fix webdriver

--- conftest.py
+++ conftest.py
@@ -1,21 +1,37 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options as OptionsFirefox
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default="en",
                      help="Choose language: en")
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: ['--browser_name=safari'\n'--browser_name=chrome'\n'--browser_name=firefox'")
 
 @pytest.fixture(scope="function")
 def browser(request):
+    browser_name = request.config.getoption("browser_name")
     language = request.config.getoption("language")
     browser = None
 
-    if language == "en":
-        browser = webdriver.Safari()      # If you user of MacOS
-        # browser = webdriver.Chrome()    # If not Choose Chrome or other webdrivers
-        print(f"\nstart {language}")
+    if browser_name == "safari":
+        options = webdriver.safari.options.Options()
+        browser = webdriver.Safari(options=options)
+        print("\nStarting Safari..")
+    elif browser_name == "chrome":
+        options = Options()
+        options.add_experimental_option(
+            'prefs', {'intl.accept_languages': language})
+        print("\nStarting Сhrome..")
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        options_firefox = OptionsFirefox()
+        options_firefox.set_preference("intl.accept_languages", language)
+        print("\nStarting Firefox..")
+        browser = webdriver.Firefox(options=options_firefox)
     else:
-        raise pytest.UsageError("--language should be: en")
+        raise pytest.UsageError("--browser_name should be chrome, firefox or safari")
 
     yield browser
     print("\nquit..")
